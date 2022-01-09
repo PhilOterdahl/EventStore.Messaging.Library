daf3811206c9738e61d22273effb7a723a1e1ec6
Philip Oterdahl <tintin.oterdahl@hotmail.com>
2022-01-09 20:10:16 +0100

    Add test for streamId

---
+++ test/EventStore.Library.Tests.Core/StreamIdTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EventStore.Library.Tests.Core.TestAggregates;
+using FluentAssertions;
+using Xunit;
+
+namespace EventStore.Library.Tests.Core;
+
+public class StreamIdTests
+{
+    [Fact]
+    public void Stream_id_can_not_be_null()
+    {
+        Assert.Throws<ArgumentNullException>(() => new UserId(null));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    public void Stream_id_can_not_be_whitespace_or_empty(string id)
+    {
+        Assert.Throws<ArgumentException>(() => new UserId(id));
+    }
+
+    [Fact]
+    public void Stream_id_writes_category_dash_id_when_calling_to_string()
+    {
+        var id = UserId.Create();
+        id.ToString().Should().StartWith(id.Category);
+    }
+}
